767af97ccab82e5382b4fa0edac1719ea279040b
AndreaHonegger <ah@triarc-labs.com>
2021-03-10 17:40:15 +0100

    feature/updated software-section

--- src/Services.svelte
+++ src/Services.svelte
@@ -1,10 +1,9 @@
 <div class="py-24 my-16 bg-gray-200" id="services">
     <div class="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="lg:text-center">
-            <p class="text-base leading-6 text-indigo-600 font-semibold tracking-wide uppercase">Dienstleistung
-            </p>
+
             <h3 class="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-700 sm:text-4xl sm:leading-10">
-                SOFTWAREENTWICKLUNG
+                CUSTOM-SOFTWAREENTWICKLUNG
             </h3>
             <p class="mt-4 max-w-2xl text-xl leading-7 text-gray-500 lg:mx-auto">
                 damit Sie Ihre Informationen verständlich und überall zur Hand haben
@@ -16,27 +15,9 @@
                 <li>
                     <div class="flex">
                         <div class="flex-shrink-0">
-                            <div class="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
-                                <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9"/>
-                                </svg>
-                            </div>
-                        </div>
-                        <div class="ml-4">
-                            <h4 class="text-lg leading-6 font-medium text-gray-700">Individuell
-                            </h4>
-                            <p class="mt-2 text-base leading-6 text-gray-600">
-                                Unsere Web- und Mobilapplikationen sind so vielfältig wie Ihre Bedürfnisse.
-                            </p>
-                        </div>
-                    </div>
-                </li>
-                <li class="mt-10 md:mt-0">
-                    <div class="flex">
-                        <div class="flex-shrink-0">
-                            <div class="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
-                                <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 6l3 1m0 0l-3 9a5.002 5.002 0 006.001 0M6 7l3 9M6 7l6-2m6 2l3-1m-3 1l-3 9a5.002 5.002 0 006.001 0M18 7l3 9m-3-9l-6-2m0-2v2m0 16V5m0 16H9m3 0h3"/>
+                            <div class="flex items-center justify-center h-12 w-12 p-3 rounded-md bg-indigo-500 text-white p2">
+                                <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="magic" class="svg-inline--fa fa-magic fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                                    <path fill="currentColor" d="M224 96l16-32 32-16-32-16-16-32-16 32-32 16 32 16 16 32zM80 160l26.66-53.33L160 80l-53.34-26.67L80 0 53.34 53.33 0 80l53.34 26.67L80 160zm352 128l-26.66 53.33L352 368l53.34 26.67L432 448l26.66-53.33L512 368l-53.34-26.67L432 288zm70.62-193.77L417.77 9.38C411.53 3.12 403.34 0 395.15 0c-8.19 0-16.38 3.12-22.63 9.38L9.38 372.52c-12.5 12.5-12.5 32.76 0 45.25l84.85 84.85c6.25 6.25 14.44 9.37 22.62 9.37 8.19 0 16.38-3.12 22.63-9.37l363.14-363.15c12.5-12.48 12.5-32.75 0-45.24zM359.45 203.46l-50.91-50.91 86.6-86.6 50.91 50.91-86.6 86.6z"></path>
                                 </svg>
                             </div>
                         </div>
@@ -44,7 +25,7 @@
                             <h4 class="text-lg leading-6 font-medium text-gray-700">Massgeschneidert
                             </h4>
                             <p class="mt-2 text-base leading-6 text-gray-600">
-                                Wir setzen Ihre Arbeitsprozesse schlank um, so dass jeder Mitarbeiter jederzeit und überall komplexe Arbeiten selbständig erledigen kann. Offlinefähig zu sein erlaubt es Ihnen auch Mitarbeiter in verbindungsfeindlicher Umgebung mit den relevanten Informationen zu versorgen und deren Arbeiten einzubeziehen.
+                                Damit die Digitalisierung den gewünschten Effekt voll entfaltet, muss die Software-Lösung exakt ihren Bedürfnissen entsprechen. Basierend auf der Realität ihres Alltags erarbeiten wir zusammen Anforderungen und Ideen und stellen dabei den Nutzer ins Zentrum. Wir testen und passen das Produkt solange an, bis es perfekt sitzt.
                             </p>
                         </div>
                     </div>
@@ -52,17 +33,17 @@
                 <li class="mt-10 md:mt-0">
                     <div class="flex">
                         <div class="flex-shrink-0">
-                            <div class="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
-                                <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 10V3L4 14h7v7l9-11h-7z"/>
+                            <div class="flex items-center justify-center h-12 w-12 p-3 rounded-md bg-indigo-500 text-white">
+                                <svg fill="white" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="industry" class="svg-inline--fa fa-industry fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                                    <path fill="currentColor" d="M475.115 163.781L336 252.309v-68.28c0-18.916-20.931-30.399-36.885-20.248L160 252.309V56c0-13.255-10.745-24-24-24H24C10.745 32 0 42.745 0 56v400c0 13.255 10.745 24 24 24h464c13.255 0 24-10.745 24-24V184.029c0-18.917-20.931-30.399-36.885-20.248z"></path>
                                 </svg>
                             </div>
                         </div>
                         <div class="ml-4">
-                            <h4 class="text-lg leading-6 font-medium text-gray-700">Völlständig integriert
+                            <h4 class="text-lg leading-6 font-medium text-gray-700">Branchenunabhängig
                             </h4>
                             <p class="mt-2 text-base leading-6 text-gray-600">
-                                Das volle Potenzial einer Mobilapplikation entfaltet sich mit der Integration in Ihr Software-Ökosystem. Angebunden an Ihr ERP (Enterprise Resource Planning) entfallen die Medienbrüche und Prozesse können nahtlos ineinandergreifen. Zu diesem Thema bringen wir bereits viel Erfahrung in der Zusammenarbeit mit ABACUS und SAP mit.
+                                Unsere Kundschaft ist breit gefächert über viele Branchen verteilt. Dank unseres Portfolios konnten wir uns ein branchenübergreifendes Knowhow erarbeiten und lassen dies in zukünftige Projekte fliessen. Mit Leidenschaft arbeiten wir uns rasch und kompetent in neue Fachgebiete ein.
                             </p>
                         </div>
                     </div>
@@ -70,17 +51,35 @@
                 <li class="mt-10 md:mt-0">
                     <div class="flex">
                         <div class="flex-shrink-0">
-                            <div class="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
-                                <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z"/>
+                            <div class="flex items-center justify-center h-12 w-12 p-3 rounded-md bg-indigo-500 text-white">
+                                <svg fill="white" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="project-diagram" class="svg-inline--fa fa-project-diagram fa-w-20" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512">
+                                    <path fill="currentColor" d="M384 320H256c-17.67 0-32 14.33-32 32v128c0 17.67 14.33 32 32 32h128c17.67 0 32-14.33 32-32V352c0-17.67-14.33-32-32-32zM192 32c0-17.67-14.33-32-32-32H32C14.33 0 0 14.33 0 32v128c0 17.67 14.33 32 32 32h95.72l73.16 128.04C211.98 300.98 232.4 288 256 288h.28L192 175.51V128h224V64H192V32zM608 0H480c-17.67 0-32 14.33-32 32v128c0 17.67 14.33 32 32 32h128c17.67 0 32-14.33 32-32V32c0-17.67-14.33-32-32-32z"></path>
                                 </svg>
                             </div>
                         </div>
                         <div class="ml-4">
-                            <h4 class="text-lg leading-6 font-medium text-gray-700">Daten richtig nutzen
+                            <h4 class="text-lg leading-6 font-medium text-gray-700">Integriert
                             </h4>
                             <p class="mt-2 text-base leading-6 text-gray-600">
-                                Mit spezifischen Visualisierungen machen wir Ihre grossen Datenmengen verständlich und dank intelligenter Indexierung deren Details auffindbar.
+                                Das volle Potenzial von (Mobil-)Applikationen entfaltet sich nur mit der Integration in ihr Software-Ökosystem. Angebunden an Ihr ERP-System (Enterprise Ressource Planning) vermeiden sie Medienbrüche und Prozesse greifen nahtlos ineinander. Dank der Integration in Echtzeit arbeiten zahlreiche Anwender mehrerer Systeme Hand in Hand. Daten können in Echtzeit zwischen den verschiedenen Systemen getauscht werden. Wir bringen bereits viel Erfahrung für die Zusammenarbeit mit Abacus, SAP und Dynamics mit.
+                            </p>
+                        </div>
+                    </div>
+                </li>
+                <li class="mt-10 md:mt-0">
+                    <div class="flex">
+                        <div class="flex-shrink-0">
+                            <div class="flex items-center justify-center h-12 w-12 p-3 rounded-md bg-indigo-500 text-white">
+                                <svg fill="white" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="shipping-fast" class="svg-inline--fa fa-shipping-fast fa-w-20" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512">
+                                    <path fill="currentColor" d="M624 352h-16V243.9c0-12.7-5.1-24.9-14.1-33.9L494 110.1c-9-9-21.2-14.1-33.9-14.1H416V48c0-26.5-21.5-48-48-48H112C85.5 0 64 21.5 64 48v48H8c-4.4 0-8 3.6-8 8v16c0 4.4 3.6 8 8 8h272c4.4 0 8 3.6 8 8v16c0 4.4-3.6 8-8 8H40c-4.4 0-8 3.6-8 8v16c0 4.4 3.6 8 8 8h208c4.4 0 8 3.6 8 8v16c0 4.4-3.6 8-8 8H8c-4.4 0-8 3.6-8 8v16c0 4.4 3.6 8 8 8h208c4.4 0 8 3.6 8 8v16c0 4.4-3.6 8-8 8H64v128c0 53 43 96 96 96s96-43 96-96h128c0 53 43 96 96 96s96-43 96-96h48c8.8 0 16-7.2 16-16v-32c0-8.8-7.2-16-16-16zM160 464c-26.5 0-48-21.5-48-48s21.5-48 48-48 48 21.5 48 48-21.5 48-48 48zm320 0c-26.5 0-48-21.5-48-48s21.5-48 48-48 48 21.5 48 48-21.5 48-48 48zm80-208H416V144h44.1l99.9 99.9V256z"></path>
+                                </svg>
+                            </div>
+                        </div>
+                        <div class="ml-4">
+                            <h4 class="text-lg leading-6 font-medium text-gray-700">Agil
+                            </h4>
+                            <p class="mt-2 text-base leading-6 text-gray-600">
+                                In der agilen Softwareentwicklung arbeiten wir eng mit Ihnen zusammen, halten den Arbeitsprozess schlank und beurteilen gemeinsam in regelmässigen Intervallen den Zwischenstand unserer Arbeit. So sind Sie jederzeit auf dem neuesten Stand und wir können schnell und flexibel auf neue Erkenntnisse und Anforderungen reagieren. Dank dem iterativen Vorgehen haben Sie stets die Kontrolle (überarbeiten Urban)
                             </p>
                         </div>
                     </div>
