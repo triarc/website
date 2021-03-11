a1408badfdceaec15678abfae286d137b653eeb5
AndreaHonegger <ah@triarc-labs.com>
2021-03-11 11:26:52 +0100

    feature/updated team member list and added triarc colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,11 @@ module.exports = {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
+      colors: {
+        'red-triarc': '#C8102E',
+        'green-triarc': '#009639',
+        'blue-triarc': '#0077c8',
+      },
     },
   },
   plugins: [
