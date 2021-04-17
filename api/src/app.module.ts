e3ea90996676323270cfc012592e5853262d8eef
Pascal Bertschi <pascal@bertschi.io>
2021-04-17 20:55:01 +0200

    add root controller

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common'
 import { ChatController } from './chat.controller'
 import { ChatService } from './chat.service'
 import { ConfigModule } from '@nestjs/config'
+import { RootController } from './root.controller';
 
 @Module({
   imports: [ConfigModule.forRoot()],
-  controllers: [ChatController],
+  controllers: [ChatController, RootController],
   providers: [ChatService],
 })
 export class AppModule {}
---
+++ api/src/root.controller.ts
@@ -0,0 +1,9 @@
+import { Controller, Get } from '@nestjs/common';
+
+@Controller()
+export class RootController {
+	@Get('')
+	async get() {
+		return 'hello :), looking for a job? checkout https://triarc-labs.com/jobs';
+	}
+}
