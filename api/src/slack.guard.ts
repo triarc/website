9ee6eefd00105fcfb82236471f92f02e46fbdd88
Pascal Bertschi <pascal@bertschi.io>
2021-04-17 20:49:04 +0200

    fix slack signature verify

--- api/src/slack.guard.ts
+++ api/src/slack.guard.ts
@@ -1,5 +1,4 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common'
-import * as qs from 'qs'
 import * as crypto from 'crypto'
 import { Buffer } from 'buffer'
 import { ConfigService } from '@nestjs/config'
@@ -15,7 +14,6 @@ export class SlackGuard implements CanActivate {
     const { headers, body } = context.switchToHttp().getRequest()
     const hmac = crypto.createHmac('sha256', this.slackSigningSecret)
 
-    const rawBody = qs.stringify(body, { format: 'RFC1738' })
     const slackSignature = headers['x-slack-signature']
     const requestTimestamp = headers['x-slack-request-timestamp']
 
@@ -23,17 +21,18 @@ export class SlackGuard implements CanActivate {
       return false
     }
 
+    const [version, hash] = slackSignature.split('=');
+    const base = `${version}:${requestTimestamp}:${JSON.stringify(body)}`;
+    const mySign = hmac.update(base, 'utf8').digest('hex');
+
     const timeInSeconds = Math.floor(new Date().getTime() / 1000)
     // Reject if request is older than 5 minutes
     if (Math.abs(timeInSeconds - requestTimestamp) > 300) {
       return false
     }
 
-    const signatureBaseString = `v0:${requestTimestamp}:${rawBody}`
-    const mySignature = 'v0=' + hmac.update(signatureBaseString, 'utf8').digest('hex')
-
     try {
-      return crypto.timingSafeEqual(Buffer.from(mySignature, 'utf8'), Buffer.from(slackSignature, 'utf8'))
+      return crypto.timingSafeEqual(Buffer.from(mySign, 'utf8'), Buffer.from(hash, 'utf8'))
     } catch {
       return false
     }
