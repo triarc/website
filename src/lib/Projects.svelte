3e03b095df8710daa58d34d17df0e1a22bc56c2c
Pascal Bertschi <pascal@bertschi.io>
2021-04-18 19:12:42 +0200

    add verba story

--- src/lib/Projects.svelte
+++ src/lib/Projects.svelte
@@ -110,7 +110,48 @@
         <div>
           <div class="mt-6">
             <h2 class="text-3xl font-extrabold tracking-tight text-gray-900">
-              myWALO
+              Verba
+            </h2>
+            <p class="mt-4 text-lg text-gray-500">
+              Für die Verwaltung von Therapien und deren Mitglieder. Von der digitalen Krankenkassenrechnung bis ins SAGE integriert.
+              Die Vereinslösung in der Schweizer Cloud, damit sich das Bechterew-Team auf die wichtigen Dinge im Alltag fokusieren kann.
+            </p>
+          </div>
+        </div>
+        <div class="mt-8 border-t border-gray-200 pt-6">
+          <blockquote>
+            <div>
+              <p class="text-base text-gray-500">
+                &ldquo;Cras velit quis eros eget rhoncus lacus ultrices sed diam. Sit orci risus aenean curabitur donec aliquet. Mi venenatis in euismod ut.&rdquo;
+              </p>
+            </div>
+            <footer class="mt-3">
+              <div class="flex items-center space-x-3">
+                <div class="flex-shrink-0">
+                  <img class="h-6 w-6 rounded-full" src="img/svmb.png" alt="svmb">
+                </div>
+                <div class="text-base font-medium text-gray-700">
+                  René Bräm, Geschäftsleitung
+                </div>
+              </div>
+            </footer>
+          </blockquote>
+        </div>
+      </div>
+      <div class="mt-12 sm:mt-16 lg:mt-0">
+        <div class="pl-4 -mr-48 sm:pl-6 md:-mr-16 lg:px-0 lg:m-0 lg:relative lg:h-full">
+          <img class="w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5 lg:absolute lg:left-0 lg:h-full lg:w-auto lg:max-w-none" src="img/svmb.png" alt="verba">
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="mt-24">
+    <div class="lg:mx-auto lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-2 lg:grid-flow-col-dense lg:gap-24">
+      <div class="px-4 max-w-xl mx-auto sm:px-6 lg:py-32 lg:max-w-none lg:mx-0 lg:px-0 lg:col-start-2">
+        <div>
+          <div class="mt-6">
+            <h2 class="text-3xl font-extrabold tracking-tight text-gray-900">
+              myWalo
             </h2>
             <p class="mt-4 text-lg text-gray-500">
               Auf der Baustelle treffen verschiedene analoge und digitale Prozesse aufeinander. Mit Data Lake
@@ -132,16 +173,16 @@
                   <img class="h-6 w-6 rounded-full" src="https://images.unsplash.com/photo-1509783236416-c9ad59bae472?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80" alt="">
                 </div>
                 <div class="text-base font-medium text-gray-700">
-                  Marcia Hill, Digital Marketing Manager
+                  René Bräm, Geschäftsleitung SVMB
                 </div>
               </div>
             </footer>
           </blockquote>
         </div>
       </div>
-      <div class="mt-12 sm:mt-16 lg:mt-0">
-        <div class="pl-4 -mr-48 sm:pl-6 md:-mr-16 lg:px-0 lg:m-0 lg:relative lg:h-full">
-          <img class="w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5 lg:absolute lg:left-0 lg:h-full lg:w-auto lg:max-w-none" src="https://tailwindui.com/img/component-images/inbox-app-screenshot-1.jpg" alt="Inbox user interface">
+      <div class="mt-12 sm:mt-16 lg:mt-0 lg:col-start-1">
+        <div class="pr-4 -ml-48 sm:pr-6 md:-ml-16 lg:px-0 lg:m-0 lg:relative lg:h-full">
+          <img class="w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5 lg:absolute lg:right-0 lg:h-full lg:w-auto lg:max-w-none" src="img/walo-app.png" alt="mywalo">
         </div>
       </div>
     </div>
