7c73dea478c9add4b8c318272ef626c7f8ab413b
Pascal Bertschi <pascal@bertschi.io>
2021-04-18 18:46:13 +0200

    add wunder screenshot

--- src/lib/Projects.svelte
+++ src/lib/Projects.svelte
@@ -46,7 +46,7 @@
                   <img class="h-6 w-6 rounded-full" src="https://images.unsplash.com/photo-1509783236416-c9ad59bae472?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80" alt="">
                 </div>
                 <div class="text-base font-medium text-gray-700">
-                  Marcia Hill, Digital Marketing Manager
+                  Mirko Baur, Gesamtleiter Tanne
                 </div>
               </div>
             </footer>
@@ -75,17 +75,31 @@
               Auftragseröffnung über die Bearbeitung sowie Begleitung eines Kunden bis hin zum digitalisierten
               Bestellwesen
             </p>
-            <div class="mt-6">
-              <a href="#" class="inline-flex px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700">
-                Get started
-              </a>
-            </div>
           </div>
+        </div>
+        <div class="mt-8 border-t border-gray-200 pt-6">
+          <blockquote>
+            <div>
+              <p class="text-base text-gray-500">
+                &ldquo;Cras velit quis eros eget rhoncus lacus ultrices sed diam. Sit orci risus aenean curabitur donec aliquet. Mi venenatis in euismod ut.&rdquo;
+              </p>
+            </div>
+            <footer class="mt-3">
+              <div class="flex items-center space-x-3">
+                <div class="flex-shrink-0">
+                  <img class="h-6 w-6 rounded-full" src="https://images.unsplash.com/photo-1509783236416-c9ad59bae472?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80" alt="">
+                </div>
+                <div class="text-base font-medium text-gray-700">
+                  Patrick Rohrer, GL Ortho-Team
+                </div>
+              </div>
+            </footer>
+          </blockquote>
         </div>
       </div>
       <div class="mt-12 sm:mt-16 lg:mt-0 lg:col-start-1">
         <div class="pr-4 -ml-48 sm:pr-6 md:-ml-16 lg:px-0 lg:m-0 lg:relative lg:h-full">
-          <img class="w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5 lg:absolute lg:right-0 lg:h-full lg:w-auto lg:max-w-none" src="img/tanne-app.png" alt="tanne app">
+          <img class="w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5 lg:absolute lg:right-0 lg:h-full lg:w-auto lg:max-w-none" src="img/wunder-app.png" alt="tanne app">
         </div>
       </div>
     </div>
