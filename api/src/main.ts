b38eaca7cea37358117cd8caa23ef1f080220574
Pascal Bertschi <pascal@bertschi.io>
2021-04-18 11:55:17 +0200

    add cors to chatbot api

--- api/src/main.ts
+++ api/src/main.ts
@@ -4,6 +4,10 @@ import { AppModule } from './app.module'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   const PORT = process.env.PORT || 3000
+  app.enableCors({
+    origin: ['dev.triarc-labs.com', 'triarc-labs.com'],
+    credentials: false,
+  })
   app.listen(PORT).then(() => console.log('> Server listening at http://localhost:' + PORT))
 }
 bootstrap()
