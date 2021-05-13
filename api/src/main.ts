e02738eb2d88d88df268ac4be867d156c66b9d30
Pascal Bertschi <pascal@bertschi.io>
2021-05-13 09:14:11 +0200

    add sitemap

--- api/src/main.ts
+++ api/src/main.ts
@@ -4,10 +4,7 @@ import { AppModule } from './app.module'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   const PORT = process.env.PORT || 3000
-  app.enableCors({
-    origin: ['https://dev.triarc-labs.com', 'https://triarc-labs.com'],
-    credentials: false,
-  })
+  app.enableCors()
   app.listen(PORT).then(() => console.log('> Server listening at http://localhost:' + PORT))
 }
 bootstrap()
