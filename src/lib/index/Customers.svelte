7989116852dcb3955909e9fae5ae4ea1c86f9e99
Pascal Bertschi <pb@triarc-labs.com>
2022-08-11 15:29:10 +0200

    prettier

--- src/lib/index/Customers.svelte
+++ src/lib/index/Customers.svelte
@@ -109,21 +109,27 @@
         target="_blank"
         class="col-span-1 flex items-center justify-center py-8 px-8 filter"
       >
-        <img height="80" class="max-h-12" src="/img/customer/christen.svg" loading="lazy" alt="volvo" />
+        <img height="80" class="max-h-12" src="/img/customer/christen.svg" loading="lazy" alt="christen ag" />
       </a>
       <a
         href="https://www.burkhalter.ch/"
         target="_blank"
         class="col-span-1 flex items-center justify-center py-8 px-8 filter"
       >
-        <img height="80" class="max-h-12" src="/img/customer/burkhalter_holding_logo.svg" loading="lazy" alt="volvo" />
+        <img
+          height="80"
+          class="max-h-12"
+          src="/img/customer/burkhalter_holding_logo.svg"
+          loading="lazy"
+          alt="burkhalter holding group"
+        />
       </a>
       <a
         href="https://www.renggli.swiss"
         target="_blank"
         class="col-span-1 flex items-center justify-center py-8 px-8 filter"
       >
-        <img height="80" class="max-h-24" src="/img/customer/renggli-logo.svg" loading="lazy" alt="volvo" />
+        <img height="80" class="max-h-24" src="/img/customer/renggli-logo.svg" loading="lazy" alt="renggli" />
       </a>
 
       <a
@@ -147,7 +153,6 @@
       >
         <img height="80" class="max-h-28" src="/img/customer/schindler-logo.png" loading="lazy" alt="schindler" />
       </a>
-
     </div>
   </div>
 </div>
