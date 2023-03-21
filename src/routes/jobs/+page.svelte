250381efa9de78cba39d0d16beca30dbb2fb5879
Pascal Bertschi <pascal@bertschi.io>
2023-03-21 22:22:03 +0100

    remove jungle from jobs

--- src/routes/jobs/+page.svelte
+++ src/routes/jobs/+page.svelte
@@ -314,8 +314,12 @@
         content: 'Vertrauen ist gut, Sicherheit ist besser.',
         person: 'Bambam',
         personTitle: 'Sicherheitsinspektor',
-      },
+      }
     },
+    {
+      title: 'Immer noch hier?',
+      content: 'Du findest, du passt zu uns und kannst etwas beisteuern? Wir freuen uns auf deine Bewerbung und sehen dich in unserem Jungle.'
+    }
   ]
 </script>
 
@@ -335,22 +339,6 @@
   {#each contents as content}
     <Block bind:content />
   {/each}
-</div>
-
-
-<div class="bg-[#0D1214] min-h-[calc(100vh-64px)] flex flex-col lg:min-h-screen">
-  <div class="text-white bg-opacity-20">
-    <div class="max-w-2xl mx-auto text-center py-16 px-4 sm:py-20 sm:px-6 lg:px-8">
-      <h2 class="text-3xl font-extrabold sm:text-4xl">
-        <span class="block">Immer noch hier?</span>
-      </h2>
-      <p class="mt-4 text-lg leading-6">
-        Du findest, du passt zu uns und kannst etwas beisteuern? Wir freuen uns auf deine Bewerbung und sehen dich in
-        unserem Jungle.
-      </p>
-    </div>
-  </div>
-  <img src="img/jobs/jungle.svg" class="w-full h-full" alt="triarc jungle" id="jungle" />
 </div>
 
 <Footer></Footer>
