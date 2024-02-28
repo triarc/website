261bfdd0f171c620212e8744ca15dea7b76a1e35
nocrest <lukihartmann@bluewin.ch>
2024-02-28 10:35:04 +0100

    fix: added missing partners and customers

--- src/lib/index/Partners.svelte
+++ src/lib/index/Partners.svelte
@@ -1,7 +1,5 @@
 <div class="bg-white">
-    <div class="text-center px-16 lg:px-8 py-8 max-w-4xl lg:mr-auto md:py-16 sm:px-6" id="partners">
-
-
+    <div class="text-center max-w-screen-xl mx-auto py-24 md:py-32 px-4 sm:px-6 lg:px-8" id="partners">
         <h2 class="text-3xl leading-9 font-extrabold tracking-tight sm:text-4xl">Unsere Partner</h2>
         <div class="mt-6 grid grid-cols-2 md:grid-cols-3 lg:mt-8 customer-grid">
             <div class="customer-grid-row">
