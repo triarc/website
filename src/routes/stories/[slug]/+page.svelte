f838217629bbf4350ea8ead776a9dcfea2bb027e
nocrest <lukihartmann@bluewin.ch>
2024-03-04 16:07:44 +0100

    fix: link back to original page

--- src/routes/stories/[slug]/+page.svelte
+++ src/routes/stories/[slug]/+page.svelte
@@ -4,12 +4,15 @@
   import type { PageData } from './$types'
   export let data: PageData
   export let contactString: string = data.title
+export function nav_back() {
+        window.history.back()
+}
 </script>
 
 <div class="bg-white min-h-screen px-12 md:px-24 py-16 md:py-24 w-full">
   <div class="prose lg:prose-xl prose-img:rounded-xl prose-a:text-blue-triarc mx-auto">
     <div class="flex space-x-2 items-center bg-white mb-2">
-      <a href="/stories" class="flex items-center text-blue-triarc fill-blue-triarc">
+      <a on:click={() => nav_back()} class="flex items-center text-blue-triarc fill-blue-triarc">
         <svg xmlns="http://www.w3.org/2000/svg" class="h-6" viewBox="0 0 448 512"
           ><!--! Font Awesome Pro 6.2.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path
             d="M447.1 256c0 13.25-10.76 24.01-24.01 24.01H83.9l132.7 126.6c9.625 9.156 9.969 24.41 .8125 33.94c-9.156 9.594-24.34 9.938-33.94 .8125l-176-168C2.695 268.9 .0078 262.6 .0078 256S2.695 243.2 7.445 238.6l176-168C193 61.51 208.2 61.85 217.4 71.45c9.156 9.5 8.812 24.75-.8125 33.94l-132.7 126.6h340.1C437.2 232 447.1 242.8 447.1 256z"
