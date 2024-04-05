23d4de120c9a89c211f9f4c32df59cd67a825577
nocrest <lukihartmann@bluewin.ch>
2024-04-05 12:06:32 +0200

    fix: eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,7 +2,7 @@ module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
-  plugins: ['@typescript-eslint'],
+  plugins: ['eslint-plugin-svelte, @typescript-eslint'],
   ignorePatterns: ['*.cjs'],
   overrides: [{
     files: ['*.svelte'],
