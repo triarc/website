cbb1ff770e7bc5026d56e02307bd954598ec76e0
nocrest <lukihartmann@bluewin.ch>
2024-07-26 11:45:41 +0200

    chore: lint

--- src/ambient.d.ts
+++ src/ambient.d.ts
@@ -1,22 +1,23 @@
 // Used to make the svelte lint shut up about not recognizing imported images
-
+// Unknown type might be better but image src does not accept it so any will be used until a better solution is available
 declare module '*.jpg' {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const value: any
   export = value
 }
-
 declare module '*.png' {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const value: any
   export = value
 }
-
 declare module '*.webp' {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const value: any
   export = value
 }
-
 declare module '*.avif' {
-  const value: unknown
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const value: any
   export = value
 }
 
