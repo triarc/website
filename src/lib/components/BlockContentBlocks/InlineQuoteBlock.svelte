5df1ae8835b525a69619d439c20a79ba21acc6f8
nocrest <lukihartmann@bluewin.ch>
2024-10-21 15:16:06 +0200

    feat: initial draft for refactoring of new layout

---
+++ src/lib/components/BlockContentBlocks/InlineQuoteBlock.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import type { InlineQuote } from '$lib/components/TypeDefinitions'
+
+  export let quote: InlineQuote
+</script>
+
+<blockquote
+  class="bg-transparent border-l-4 border-l-gray-500 mx-8 p-4 rounded-xl my-6 text-gray-500"
+>
+  <!-- eslint-disable-next-line svelte/no-at-html-tags -- HTML-Content is static -->
+  {@html quote}
+</blockquote>
