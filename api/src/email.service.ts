27ac6a5552848cd429e4eb903fab890c7bdcf925
nocrest <lukihartmann@bluewin.ch>
2024-11-26 14:01:04 +0100

    feat: Added Action Buttons in Gitlab Issue feat: Replaced nodemailer with MS Graph API

--- api/src/email.service.ts
+++ api/src/email.service.ts
@@ -1,16 +1,12 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
-import * as nodemailer from 'nodemailer'
 import { IsEmail, IsNotEmpty, IsOptional, IsString } from 'class-validator'
 import { ChatService } from './chat.service'
 import { Block, KnownBlock } from '@slack/web-api'
 import { GStorageService } from './gStorage.service'
-import { Client, ClientOptions } from '@microsoft/microsoft-graph-client'
+import { Client } from '@microsoft/microsoft-graph-client'
 import { ClientSecretCredential } from '@azure/identity'
-import {
-  TokenCredentialAuthenticationProvider,
-  TokenCredentialAuthenticationProviderOptions,
-} from '@microsoft/microsoft-graph-client/lib/src/authentication/azureTokenCredentials'
+import { TokenCredentialAuthenticationProvider } from '@microsoft/microsoft-graph-client/lib/src/authentication/azureTokenCredentials'
 
 export class ApplicationFormDto {
   @IsString()
@@ -48,143 +44,58 @@ const gCloudBaseURL = 'https://storage.cloud.google.com/'
 
 @Injectable()
 export class EmailService {
-  // transporter: nodemailer.Transporter
-  // client: Client
+  client: Client
 
   constructor(
     private config: ConfigService,
     private chat: ChatService,
     private gStorage: GStorageService
   ) {
-    // const scopes = 'https://graph.microsoft.com/.default'
-    // const tokenCredential = new ClientSecretCredential(
-    //   this.config.get('AZURE_TENANT_ID'),
-    //   this.config.get('AZURE_CLIENT_ID'),
-    //   this.config.get('AZURE_CLIENT_SECRET')
-    // )
-    // const authProvider = new TokenCredentialAuthenticationProvider(tokenCredential, { scopes: [scopes] })
-    // this.client = Client.initWithMiddleware({
-    //   debugLogging: true,
-    //   authProvider,
-    // })
-    // this.transporter = nodemailer.createTransport({
-    //   host: this.config.get('SMTP_HOST'),
-    //   port: this.config.get('SMTP_PORT'),
-    //   auth: {
-    //     user: this.config.get('MAIL_USER'),
-    //     pass: this.config.get('MAIL_PASSWORD'),
-    //   },
-    // })
+    this.client = this.initializeMsGraphClient()
   }
 
   async sendMail(data: ApplicationFormDto, attachments: Array<Express.Multer.File>) {
-    const scopes = 'https://graph.microsoft.com/.default'
-    const tokenCredential = new ClientSecretCredential(
-      this.config.get('AZURE_TENANT_ID'),
-      this.config.get('AZURE_CLIENT_ID'),
-      this.config.get('AZURE_CLIENT_SECRET')
-    )
-    const authProvider = new TokenCredentialAuthenticationProvider(tokenCredential, { scopes: [scopes] })
-    const client = Client.initWithMiddleware({
-      debugLogging: true,
-      authProvider,
-    })
-
     // Gitlab Markdown needs double \n for proper Newlines
     const today = new Date()
     const bucketFolder = `${encodeURIComponent(data.firstName)}_${encodeURIComponent(data.lastName)}_${today.getDate().toString().padStart(2, '0')}-${(today.getMonth() + 1).toString().padStart(2, '0')}-${today.getFullYear()}_${today.getHours().toString().padStart(2, '0')}-${today.getMinutes().toString().padStart(2, '0')}-${today.getSeconds().toString().padStart(2, '0')}`
     const filenames = attachments.map((file) => file.originalname).join('\n')
-
     const attachmentLinks: GitlabLink[] = await this.handleAttachments(attachments, bucketFolder)
     const issueSubject = `${data.jobListing} - ${data.firstName} ${data.lastName}`
-
     const replySubject =
       `${data.jobListing === 'Initiativbewerbung' ? 'Deine Initiativbewerbung' : `Deine Bewerbung als ${data.jobListing}`}` +
       ` bei Triarc Laboratories`
-    const issueButtons = this.createResponseTemplates(data, replySubject)
+    const issueButtons = this.createResponseButtons(data, replySubject)
     const issueText = this.createGitlabIssue(data, attachmentLinks, issueButtons)
-    const replyText = this.createReply(data, filenames)
+    const replyText = this.createConfirmationText(data, filenames)
     const slackMessage = this.createSlackMessage(data)
     const slackBlocks = this.createSlackBlocks(data)
     const issueEmail = this.createIssueEmail(issueSubject, issueText)
     const confirmationEmail = this.createConfirmationEmail(data, replySubject, replyText)
-    // for (let file of attachments) {
-    //   let attachment = {
-    //     filename: file.originalname,
-    //     contentType: file.mimetype,
-    //     content: file.buffer,
-    //   }
-    //   issueEmail.attachments.push(attachment)
-    // }
-    try {
-      let response = await client.api(`/users/${this.config.get('MAIL_USER')}/sendMail`).post({ message: issueEmail })
-      // await this.transporter.sendMail(issueEmail)
-      console.log('Issue Email sent')
-    } catch (error) {
-      throw new HttpException('Internal Server Error', HttpStatus.INTERNAL_SERVER_ERROR)
-    }
-
-    try {
-      let response = await client
-        .api(`/users/${this.config.get('MAIL_USER')}/sendMail`)
-        .post({ message: confirmationEmail })
-      // this.transporter.sendMail(confirmationEmail)
-      console.log('Confirmation Email sent')
-    } catch (error) {
-      throw new HttpException('Internal Server Error', HttpStatus.INTERNAL_SERVER_ERROR)
-    }
-
-    await this.chat.postMessage('', slackMessage, slackBlocks)
+    await this.sendEmail(issueEmail, 'Internal Server Error sending issue E-Mail')
+    await this.sendEmail(confirmationEmail, 'Internal Server Error sending confirmation E-Mail')
+    await this.sendSlackMessage(slackMessage, slackBlocks)
   }
 
-  private createConfirmationEmail(data: ApplicationFormDto, replySubject: string, replyText: string) {
-    // return {
-    //   from: this.config.get('MAIL_USER'),
-    //   to: data.email,
-    //   subject: replySubject,
-    //   text: replyText,
-    // }
-    return {
-      subject: replySubject,
-      toRecipients: [
-        {
-          emailAddress: {
-            address: data.email,
-          },
-        },
-      ],
-      body: {
-        contentType: 'Text',
-        content: replyText,
-      },
-    }
+  private initializeMsGraphClient() {
+    const scopes = 'https://graph.microsoft.com/.default'
+    const tokenCredential = new ClientSecretCredential(
+      this.getConfigValue('AZURE_TENANT_ID'),
+      this.getConfigValue('AZURE_CLIENT_ID'),
+      this.getConfigValue('AZURE_CLIENT_SECRET')
+    )
+    const authProvider = new TokenCredentialAuthenticationProvider(tokenCredential, { scopes: [scopes] })
+    return Client.initWithMiddleware({
+      debugLogging: true,
+      authProvider,
+    })
   }
-
-  private createIssueEmail(issueSubject: string, issueText: string) {
-    // return {
-    //   from: this.config.get('MAIL_USER'),
-    //   to: this.config.get('GITLAB_TARGET_MAIL'),
-    //   subject: issueSubject,
-    //   text: issueText,
-    //   attachments: [],
-    // }
-
-    return {
-      subject: issueSubject,
-      toRecipients: [
-        {
-          emailAddress: {
-            address: this.config.get('GITLAB_TARGET_MAIL'),
-          },
-        },
-      ],
-      body: {
-        contentType: 'Text',
-        content: issueText,
-      },
+  private getConfigValue(key: string): string {
+    const value = this.config.get<string>(key)
+    if (!value) {
+      throw new Error(`Configuration value for ${key} not found`)
     }
+    return value
   }
-
   private async handleAttachments(attachments: Array<Express.Multer.File>, bucketFolder: string) {
     const links: GitlabLink[] = []
     for (const file of attachments) {
@@ -199,7 +110,28 @@ export class EmailService {
     }
     return links
   }
-
+  private createResponseButtons(data: ApplicationFormDto, replySubject: string) {
+    const subject = encodeURIComponent(replySubject)
+    const templateTexts = this.createResponsesFromTemplateStrings(data)
+    const bodyPositiveResponse = encodeURIComponent(templateTexts[0])
+    const bodyNegativeResponse = encodeURIComponent(templateTexts[1])
+    return [
+      `<a href="mailto:${data.email}?subject=${subject}&body=${bodyPositiveResponse}">Zu Interview Einladen</a>`,
+      `<a href="mailto:${data.email}?subject=${subject}&body=${bodyNegativeResponse}">Vorläufig Absagen</a>`,
+    ]
+  }
+  private createResponsesFromTemplateStrings(data: ApplicationFormDto) {
+    const positiveResponse =
+      `Hallo ${data.firstName},\n\n` +
+      `${
+        data.jobListing === 'Initiativbewerbung'
+          ? `Herzlichen Dank für deine Initiativbewerbung und dein Interesse an unserem Team „triarc-laboratories“. \n`
+          : `Herzlichen Dank für deine Bewerbung und dein Interesse an der Position als ${data.jobListing} in unserem Team „triarc-laboratories“.\n`
+      }` +
+      `${this.getConfigValue('MAIL_RESPONSE_POSITIVE')}`
+    const negativeResponse = `Hallo ${data.firstName},\n\n` + `${this.getConfigValue('MAIL_RESPONSE_NEGATIVE')}`
+    return [positiveResponse, negativeResponse]
+  }
   private createGitlabIssue(data: ApplicationFormDto, attachmentLinks: GitlabLink[], issueButtons: string[]) {
     return (
       `${
@@ -217,7 +149,54 @@ export class EmailService {
       `\n\n${issueButtons[0]}\n\n${issueButtons[1]}`
     )
   }
-
+  private createConfirmationText(data: ApplicationFormDto, filenames: string) {
+    return (
+      `Hallo ${data.firstName}, \n\n` +
+      `Wir haben Deine Bewerbung mit den folgenden Informationen erhalten:\n\n` +
+      `Vorname: ${data.firstName}\n` +
+      `Nachname: ${data.lastName}\n` +
+      `Email: ${data.email}\n` +
+      `Lohnvorstellung: ${data.lohn}\n` +
+      `Telefon: ${data.phone ?? '-'}\n` +
+      `Nachricht: ${data.message ?? '-'}\n\n` +
+      `Was liegt dir beim Arbeitgeber besonders am Herz: ${data.arbeitgeber ?? '-'}\n\n` +
+      `Anhang:\n` +
+      `${filenames}\n\n` +
+      `Vielen Dank für Deine Bewerbung, wir melden uns so schnell wie möglich bei Dir.`
+    )
+  }
+  private createIssueEmail(issueSubject: string, issueText: string) {
+    return {
+      subject: issueSubject,
+      toRecipients: [
+        {
+          emailAddress: {
+            address: this.getConfigValue('GITLAB_TARGET_MAIL'),
+          },
+        },
+      ],
+      body: {
+        contentType: 'Text',
+        content: issueText,
+      },
+    }
+  }
+  private createConfirmationEmail(data: ApplicationFormDto, replySubject: string, replyText: string) {
+    return {
+      subject: replySubject,
+      toRecipients: [
+        {
+          emailAddress: {
+            address: data.email,
+          },
+        },
+      ],
+      body: {
+        contentType: 'Text',
+        content: replyText,
+      },
+    }
+  }
   private createSlackBlocks(data: ApplicationFormDto) {
     const slackBlocks: Block[] | KnownBlock[] = [
       {
@@ -254,14 +233,13 @@ export class EmailService {
               text: 'Zum Board',
             },
             value: 'to_app_board',
-            url: this.config.get('GITLAB_APPLICATION_BOARD'),
+            url: this.getConfigValue('GITLAB_APPLICATION_BOARD'),
           },
         ],
       },
     ]
     return slackBlocks
   }
-
   private createSlackMessage(data: ApplicationFormDto) {
     return (
       `Neue Bewerbung über die Website:\n` +
@@ -272,45 +250,18 @@ export class EmailService {
       `Nachricht: ${data.message ?? '-'}\n\n`
     )
   }
-
-  private createReply(data: ApplicationFormDto, filenames: string) {
-    return (
-      `Hallo ${data.firstName}, \n\n` +
-      `Wir haben Deine Bewerbung mit den folgenden Informationen erhalten:\n\n` +
-      `Vorname: ${data.firstName}\n` +
-      `Nachname: ${data.lastName}\n` +
-      `Email: ${data.email}\n` +
-      `Lohnvorstellung: ${data.lohn}\n` +
-      `Telefon: ${data.phone ?? '-'}\n` +
-      `Nachricht: ${data.message ?? '-'}\n\n` +
-      `Was liegt dir beim Arbeitgeber besonders am Herz: ${data.arbeitgeber ?? '-'}\n\n` +
-      `Anhang:\n` +
-      `${filenames}\n\n` +
-      `Vielen Dank für Deine Bewerbung, wir melden uns so schnell wie möglich bei Dir.`
-    )
+  private async sendEmail(email: any, errorMessage: string) {
+    try {
+      await this.client.api(`/users/${this.getConfigValue('MAIL_USER')}/sendMail`).post({ message: email })
+    } catch (error) {
+      throw new HttpException(errorMessage, HttpStatus.INTERNAL_SERVER_ERROR)
+    }
   }
-
-  private createResponseTemplates(data: ApplicationFormDto, replySubject: string) {
-    const subject = encodeURIComponent(replySubject)
-    const templateTexts = this.createTemplateBodies(data)
-    const bodyPositiveResponse = encodeURIComponent(templateTexts[0])
-    const bodyNegativeResponse = encodeURIComponent(templateTexts[1])
-    return [
-      `<a href="mailto:${data.email}?subject=${subject}&body=${bodyPositiveResponse}">Zu Interview Einladen</a>`,
-      `<a href="mailto:${data.email}?subject=${subject}&body=${bodyNegativeResponse}">Vorläufig Absagen</a>`,
-    ]
-  }
-
-  private createTemplateBodies(data: ApplicationFormDto) {
-    const positiveResponse =
-      `Hallo ${data.firstName},\n\n` +
-      `${
-        data.jobListing === 'Initiativbewerbung'
-          ? `Herzlichen Dank für deine Initiativbewerbung und dein Interesse an unserem Team „triarc-laboratories“. \n`
-          : `Herzlichen Dank für deine Bewerbung und dein Interesse an der Position als ${data.jobListing} in unserem Team „triarc-laboratories“.\n`
-      }` +
-      `${this.config.get('MAIL_RESPONSE_POSITIVE')}`
-    const negativeResponse = `Hallo ${data.firstName},\n\n` + `${this.config.get('MAIL_RESPONSE_NEGATIVE')}`
-    return [positiveResponse, negativeResponse]
+  private async sendSlackMessage(message: string, blocks: Block[] | KnownBlock[]) {
+    try {
+      await this.chat.postMessage('', message, blocks)
+    } catch (error) {
+      throw new HttpException('Internal Server Error sending Slack Message', HttpStatus.INTERNAL_SERVER_ERROR)
+    }
   }
 }
