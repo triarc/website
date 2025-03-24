9bb2707b29e979c92f9a24d3b2c731919be7baf7
nocrest <lukihartmann@bluewin.ch>
2025-03-24 13:39:36 +0100

    fix: updated docker scripts, github actions

--- api/scripts/build-docker.sh
+++ api/scripts/build-docker.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
+export DOCKER_BUILDKIT=1
 docker build -t registry.gitlab.com/triarc-labs/webseite:$TAG .
--- api/scripts/push-docker.sh
+++ api/scripts/push-docker.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
+echo $DOCKER_PUSH_KEY | docker login registry.gitlab.com -u $DOCKER_PUSH_USER --password-stdin
 docker push registry.gitlab.com/triarc-labs/webseite:$TAG
