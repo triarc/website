ce4a71686acf3413dae7d31e9aca22d988244264
nocrest <lukihartmann@bluewin.ch>
2025-03-24 13:42:51 +0100

    chore: fix url

--- api/scripts/push-docker.sh
+++ api/scripts/push-docker.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-echo $DOCKER_PUSH_KEY | docker login registry.gitlab.com -u $DOCKER_PUSH_USER --password-stdin
+echo $DOCKER_PUSH_KEY | docker login registry.gitlab.com/triarc-labs/webseite -u $DOCKER_PUSH_USER --password-stdin
 docker push registry.gitlab.com/triarc-labs/webseite:$TAG
